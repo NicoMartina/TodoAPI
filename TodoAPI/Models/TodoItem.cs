3e1da723bcacb8d7ff621d91b79356c4fc37a437
Nicolas Martina <nicolas.martina69@gmail.com>
2024-10-03 21:52:49 +0200

    Add project files.

---
+++ TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoAPI.Models
+{
+    public class TodoItem
+    {
+        public long id { get; set; }
+        public string? Name { get; set; }
+        public bool isComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+}
---
+++ TodoAPI/Models/TodoItemDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoApi.Models;
+
+public class TodoItemDTO
+{
+    public long id { get; set; }
+    public string? Name { get; set; }
+    public bool isComplete { get; set; }
+}
